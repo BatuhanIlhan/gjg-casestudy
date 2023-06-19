d3c15cb8e77f4394bcf36c5ea7a15063c1083e58
BatuhanIlhan <mbatuhanilhan@gmail.com>
2023-06-19 12:07:57 +0300

    make point in the user table not null

--- database/migrations/20230618155302_user.up.sql
+++ database/migrations/20230618155302_user.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "user"
 (
     id              UUID PRIMARY KEY,
-    points          DOUBLE PRECISION DEFAULT 0.0,
+    points          DOUBLE PRECISION NOT NULL DEFAULT 0.0,
     country_code    VARCHAR(2),
     display_name    VARCHAR(100) NOT NULL,
     created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
